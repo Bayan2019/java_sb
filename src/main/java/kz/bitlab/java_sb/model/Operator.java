f31135ce12d6aee98b48f0627275f769bc8a293b
Bayan2019 <saparbayevabt@gmail.com>
2024-06-26 23:37:29 +0500

    Operator class

---
+++ src/main/java/kz/bitlab/java_sb/model/Operator.java
@@ -0,0 +1,31 @@
+package kz.bitlab.java_sb.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="operators")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Operator {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="name")
+    private String name;
+    @Column(name="surname")
+    private String surname;
+    @Column(name="department")
+    private String department; // IT, sales, marketing
+}
