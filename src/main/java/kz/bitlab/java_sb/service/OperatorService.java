fb5d1ecb1cefd26068b1b9e7f8c348c7b53bd3fa
Bayan2019 <saparbayevabt@gmail.com>
2024-07-03 19:50:17 +0500

    Operator

---
+++ src/main/java/kz/bitlab/java_sb/service/OperatorService.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+package kz.bitlab.java_sb.service;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import kz.bitlab.java_sb.model.Operator;
+
+/**
+ *
+ * @author bayan
+ */
+@Service
+public interface OperatorService {
+    ArrayList<Operator> getAllOperators();
+}
---
+++ src/main/java/kz/bitlab/java_sb/service/impl/OperatorServiceImpl.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package kz.bitlab.java_sb.service.impl;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kz.bitlab.java_sb.model.Operator;
+import kz.bitlab.java_sb.repository.OperatorRepository;
+import kz.bitlab.java_sb.service.OperatorService;
+
+/**
+ *
+ * @author bayan
+ */
+@Service
+public class OperatorServiceImpl implements OperatorService {
+
+    @Autowired
+    private OperatorRepository operatorRepository;
+
+    @Override
+    public ArrayList<Operator> getAllOperators() {
+        return (ArrayList<Operator>) operatorRepository.findAll();
+    }
+}
